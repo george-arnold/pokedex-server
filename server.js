c594ca487f69b3894d7111c8d58d265dc832ff4a
George Arnold <george.arnold.88@gmail.com>
2020-01-27 15:39:31 -0500

    added dataset for API

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 console.log(process.env.API_TOKEN);
 const app = express()
@@ -10,12 +11,14 @@ app.use(morgan('dev'))
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 app.use(function validateBearerToken(req, res, next) {
-  const bearerToken = req.get('Authorization').split(' ')[1];
   const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization')
+
+
   console.log('validate bearer token middleware')
   // move to the next middleware
-  if (bearerToken !== apiToken) {
-  return res.status(401).json({ error: 'Unauthorized request' })
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
   }
   next()
 })
